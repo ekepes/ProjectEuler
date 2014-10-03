1e75b303d153ca5986c326ee5dd3b03c959f3543
Eric Kepes <ekepes@gmail.com>
2014-10-02 21:34:43 -0400

    Added some comments.

--- clojure-euler/src/clojure_euler/problem_1.clj
+++ clojure-euler/src/clojure_euler/problem_1.clj
@@ -1,12 +1,27 @@
 (ns clojure-euler.problem-1
   (:gen-class))
 
-(defn is-divisible [v d] (= 0 (mod v d)))
+(defn is-divisible 
+  "determines if v id evenly divisible by d"
+  [v d] 
+  (= 0 (mod v d)))
 
-(defn is-multiple [v] (or (is-divisible v 3) (is-divisible v 5)))
+(defn is-multiple 
+  "determines if v is divisible by 3 or by 5 (or both)"
+  [v] 
+  (or (is-divisible v 3) (is-divisible v 5)))
 
-(defn get-multiples [limit] (filter is-multiple (range 1 limit)))
+(defn get-multiples 
+  "finds all of the values divisible by 3, 5, or both up to the limit"
+  [limit] 
+  (filter is-multiple (range 1 limit)))
 
-(defn find-sum [limit] (reduce + (get-multiples limit)))
+(defn find-sum 
+  "determines the sum of all values divisible by 3, 5, or both up to the limit"
+  [limit] 
+  (reduce + (get-multiples limit)))
 
-(defn solve [] (find-sum 1000))
+(defn solve 
+  "solves the Project Euler problem"
+  [] 
+  (find-sum 1000))
--- clojure-euler/src/clojure_euler/problem_2.clj
+++ clojure-euler/src/clojure_euler/problem_2.clj
@@ -1,14 +1,28 @@
 (ns clojure-euler.problem-2
   (:gen-class))
 
-(defn fibmax [max] 
+(defn fibmax
+  "creates the fibonaci series where the last element is less than the max" 
+  [max] 
   (take-while (partial > max) 
-    (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))))
+              (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))))
 
-(defn is-even [v] (= 0 (mod v 2)))
+(defn is-even
+  "returns true if v is even" 
+  [v]
+  (= 0 (mod v 2)))
 
-(defn get-evens [limit] (filter is-even (fibmax limit)))
+(defn get-evens 
+  "creates a sequence of the even values of the fibonaci series up to the limit"
+  [limit] 
+  (filter is-even (fibmax limit)))
 
-(defn find-sum [limit] (reduce + (get-evens limit)))
+(defn find-sum 
+  "finds the sum of the even values of the fibonaci series up to the limit"
+  [limit] 
+  (reduce + (get-evens limit)))
 
-(defn solve [] (find-sum 4000000))
+(defn solve 
+  "solves the Project Euler problem"
+  [] 
+  (find-sum 4000000))

d6f44fdfbe57753f114c0927706e948de2cef471
Eric Kepes <ekepes@gmail.com>
2015-02-07 21:17:44 -0500

    Problem #6 in F# done.

---
+++ FEuler/Solutions/Euler006.fsx
@@ -0,0 +1,10 @@
+﻿let SumOfSquares max =
+    Seq.reduce (fun acc i -> acc + i * i) {1..max}
+
+let SquareOfSums max =
+    pown (Seq.reduce (fun acc i -> acc + i) {1..max}) 2
+
+let SumSquareDifference max =
+    SquareOfSums max - SumOfSquares max
+
+SumSquareDifference 100

40135a005c7c28aba98fe6cdd845eb88a6923a49
Eric Kepes <eric@kepes.net>
2016-01-03 13:12:44 -0500

    Solved problem 4 in Python

---
+++ python/p004.py
@@ -0,0 +1,24 @@
+def reverse(value):
+  return int(str(value)[::-1])
+
+def isPalindrome(value):
+  return value == reverse(value)
+
+def largestNumberWithDigits(value):
+  result = ""
+  for x in range(value):
+    result = result + "9"
+  return int(result)
+
+def solve(value):
+  start = largestNumberWithDigits(value)
+  finish = int(start / 2)
+  result = 0
+
+  for first in range(start, finish, -1):
+    for second in range(start, finish, -1):
+      trial = first * second
+      if isPalindrome(trial) and trial > result:
+        result = trial
+
+  return result

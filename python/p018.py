59b93db199d976a92187c7e4c2bfc2e45bb91c22
Eric Kepes <eric@kepes.net>
2016-01-16 14:26:52 -0500

    Removed some unneeded code.

--- python/p018.py
+++ python/p018.py
@@ -24,33 +24,19 @@ pyramid = ["75",
 
 class Node(object):
   def __init__(self, weight, row, column, nodes):
-    self.weight = int(weight)
+    left = 0
+    right = 0
     if row > 0:
       if column < row:
-        self.right = getKey(row-1, column)
-      else:
-        self.right = None
+        right = int(nodes[getKey(row-1, column)].getCumWeight())
       if column > 0:
-        self.left = getKey(row-1, column-1)
-      else:
-        self.left = None
-    else:
-      self.left = None
-      self.right = None
-    self.cumWeight = self.weight + max(resolveNode(nodes, self.left), resolveNode(nodes, self.right))
+        left = int(nodes[getKey(row-1, column-1)].getCumWeight())
+    self.cumWeight = int(weight) + max(left, right)
   def getCumWeight(self):
     return self.cumWeight
-  def __str__(self):
-     return str(self.weight) + ":" + str(self.left) + "," + str(self.right) + "=" + str(self.cumWeight)
 
 def getKey(row, column):
   return row * 1000 + column
-
-def resolveNode(nodes, key):
-  if key == None:
-    return 0
-  else:
-    return int(nodes[key].getCumWeight())
 
 def parseLine(row, line, nodes):
   lineNodes = {}

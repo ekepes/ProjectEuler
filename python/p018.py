7b8a82ff4699ee10019660bf10e46ce7530d2815
Eric Kepes <eric@kepes.net>
2016-01-15 21:37:50 -0500

    Problem #18 - heading in the right direction Code works for the sample data, but is ugly. Need to refine.

--- python/p018.py
+++ python/p018.py
@@ -4,47 +4,51 @@ pyramid = ["3",
   "8 5 9 3"]
 
 class Node(object):
-  def __init__(self, weight, row, column, maxRows):
-    self.weight = weight
-    if row + 1 < maxRows:
-      self.left = getKey(row+1, column)
-      self.right = getKey(row+1, column+1)
+  def __init__(self, weight, row, column, nodes):
+    self.weight = int(weight)
+    if row > 0:
+      if column < row:
+        self.right = getKey(row-1, column)
+      else:
+        self.right = None
+      if column > 0:
+        self.left = getKey(row-1, column-1)
+      else:
+        self.left = None
     else:
       self.left = None
       self.right = None
-    self.row = row
-    self.column = column
-  def getWeigth(self):
-    return self.weight
-  def getLeft(self):
-    return self.left
-  def getRight(self):
-    return self.right
-  def isTerminal(self):
-    return self.left == None
+    self.cumWeight = self.weight + max(resolveNode(nodes, self.left), resolveNode(nodes, self.right))
+  def getCumWeight(self):
+    return self.cumWeight
   def __str__(self):
-     return str(self.weight) + ":" + str(self.left) + "," + str(self.right)
+     return str(self.weight) + ":" + str(self.left) + "," + str(self.right) + "=" + str(self.cumWeight)
 
 def getKey(row, column):
   return row * 1000 + column
 
-def parseLine(row, line, maxRows):
+def resolveNode(nodes, key):
+  if key == None:
+    return 0
+  else:
+    return int(nodes[key].getCumWeight())
+
+def parseLine(row, line, nodes):
   lineNodes = {}
   column = 0
   for item in line.split():
-    lineNodes[getKey(row, column)] = Node(item, row, column, maxRows)
+    lineNodes[getKey(row, column)] = Node(item, row, column, nodes)
     column += 1
   return lineNodes
 
 def parsePyramid(pyramid):
   nodes = {}
   row = 0
-  maxRows = len(pyramid)
   for line in pyramid:
-    nodes.update(parseLine(row, line, maxRows))
+    nodes.update(parseLine(row, line, nodes))
     row += 1
   return nodes
 
 nodes = parsePyramid(pyramid)
 for n in nodes:
-  print(n, nodes[n], nodes[n].isTerminal())
+  print(n, nodes[n])

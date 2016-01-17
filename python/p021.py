26539f67bc258d2d08d00d1253e213637de537bd
Eric Kepes <eric@kepes.net>
2016-01-17 15:09:17 -0500

    solved problem #21

---
+++ python/p021.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+def factors(n):
+  result = set(reduce(list.__add__,
+    ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+  result.remove(n)
+  return result
+
+def sumOfFactors(n):
+  total = 0
+  for i in factors(n):
+    total += i
+  return total
+
+
+total = 0
+for i in range(2, 10000):
+  x = sumOfFactors(i)
+  if x != i:
+    y = sumOfFactors(x)
+    if (y == i):
+      total += i
+
+print(total)

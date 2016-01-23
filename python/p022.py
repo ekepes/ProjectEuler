2a6d0e176b51e2e2338cfbff96fea25682d70451
Eric Kepes <eric@kepes.net>
2016-01-22 21:28:21 -0500

    Problem #22 solved.

--- python/p022.py
+++ python/p022.py
@@ -1,9 +1,23 @@
 import time
 startTime = time.time()
 
-f = open('p022_names.txt', 'r')
-data = f.read()
+def scoreName(name):
+  total = 0
+  for c in name:
+    total += ord(c) - 64
+  return total
 
-print(data)
+f = open('p022_names.txt', 'r')
+data = f.read().replace("\"","")
+names = data.split(",")
+names.sort()
+
+count = 1
+total = 0
+for name in names:
+  total += scoreName(name) * count
+  count += 1
+
+print(total)
 
 print("Run Time = " + str(time.time() - startTime))

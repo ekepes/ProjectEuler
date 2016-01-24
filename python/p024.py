6a7fb6ea893716740210575fdffd44756be5ce0c
Eric Kepes <eric@kepes.net>
2016-01-24 14:29:01 -0500

    Solved problem #24.

--- python/p024.py
+++ python/p024.py
@@ -2,16 +2,24 @@ import util
 import time
 startTime = time.time()
 
-digits = ["0", "1", "2"]
-permutations = []
-for digit1 in digits:
-  remaining1 = digits[:]
-  remaining1.remove(digit1)
-  for digit2 in remaining1:
-    remaining2 = remaining1[:]
-    remaining2.remove(digit2)
-    for digit3 in remaining2:
-      print(digit1 + digit2 + digit3)
+digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+count = 0
+desired = 1000000
 
+def blah(soFar, remainingDigits):
+  global count, desired
+  if len(remainingDigits) == 0:
+    count += 1
+    if count == desired:
+      print(soFar)
+  else:
+    for digit in remainingDigits:
+      rest = remainingDigits[:]
+      rest.remove(digit)
+      blah(soFar + digit, rest)
+
+blah("", digits)
+
+print(count)
 
 print("Run Time = " + str(time.time() - startTime))
